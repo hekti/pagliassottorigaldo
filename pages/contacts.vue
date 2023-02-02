51504032601c74465017a9d39f365509e6a09ca3
emanuele-em <micheletti.emanuele@hotmail.com>
2023-02-02 23:38:03 +0100

    add content

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -5,7 +5,7 @@
         <Info :title="`I nostri contatti`">
             <ul class="w-fit mx-auto mt-5">
                 <li>
-                    <span class="material-symbols-rounded">call</span> <a href="tel:+393427003628">+39 342 700 3628</a>
+                    <span class="material-symbols-rounded">call</span> <a href="tel:+390115171224">+39 011 517 1224</a>
                 </li>
                 <li>
                     <span class="material-symbols-rounded">mail</span> <a
@@ -16,8 +16,7 @@
                         Vinzaglio, 29</a>
                 </li>
                 <li>
-                    <span class="material-symbols-rounded">schedule</span> <span> <b>Chiuso</b> Apre domani alle
-                        9:00</span>
+                    <span class="material-symbols-rounded">schedule</span> <span ref="el" ></span>
                 </li>
 
             </ul>
@@ -26,6 +25,7 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 
 const conf = {
     'bgLight': true,
@@ -33,6 +33,82 @@ const conf = {
     'bgGrad': ["darkGreen", "lightGreen"],
 }
 
+const hours = [
+    [],
+    [9, 13, 14, 18],
+    [9, 13, 14, 18],
+    [9, 13, 14, 18],
+    [9, 13, 14, 18],
+    [9, 13, 14, 18],
+    [],
+]
+
+var date = new Date();
+var today = date.getDay();
+var current_hour = date.getHours() + date.getMinutes() / 60;
+
+const weekDay = (i) => {
+    switch (i) {
+        case 0: return "Domenica";
+        case 1: return "Lunedì";
+        case 2: return "Martedì";
+        case 3: return "Mercoledì";
+        case 4: return "Giovedì";
+        case 5: return "Venerdì";
+        case 6: return "Sabato";
+        default: return "" 
+    }
+}
+
+const is_open = () => {    
+    //check if the current hour is between opening (even positions) and closing (odd positions) hours
+    for (var i = 0; i < hours[today].length - 1; i+=2) {
+        if ((current_hour < hours[today][i+1] && current_hour > hours[today][i]) || (i % 2 != 0 && current_hour > hours[today][i])) {
+            var hh = Math.floor(hours[today][i+1]);
+            var mm = hours[today][i+1] - hh;
+            if (mm < 10) mm = "0"+mm;
+            return "<b>Aperto</b> Chiude alle "+hh+":"+mm;
+        } else if (current_hour < hours[today][i]){
+            // check if it is a infra-day break
+            var hh = Math.floor(hours[today][i]);
+            var mm = hours[today][i] - hh;
+            if (mm < 10) mm = "0"+mm;
+            return "<b>Chiuso</b> Apre alle "+hh+":"+mm;
+        }
+    }
+
+    // check if tomorrow is open
+    if( (typeof hours[today+1] !== 'undefined') && hours[today+1].length > 0){
+        var hh = Math.floor(hours[today+1][0]);
+        var mm = hours[today+1][0] - hh;
+        if (mm < 10) mm = "0"+mm;
+        return "<b>Chiuso</b> Apre domani alle "+hh+":"+mm;
+    } else if ( (typeof hours[today+1] === 'undefined') && hours[0].length > 0) {
+        var hh = Math.floor(hours[0][0]);
+        var mm = hours[0][0] - hh;
+        if (mm < 10) mm = "0"+mm;
+        return "<b>Chiuso</b> Apre domani alle "+hh+":"+mm;
+    }
+
+
+    // today is closed, find next opening
+    var i = today+2;
+    while ( (typeof hours[i] === 'undefined') || hours[i].length == 0){
+        if (typeof hours[i] === 'undefined') i = 0
+        else i += 1
+    }
+
+    var hh = Math.floor(hours[i][0]);
+    var mm = hours[i][0] - hh;
+    if (mm < 10) mm = "0"+mm;
+    return "<b>Chiuso</b> Apre "+weekDay(i)+" alle "+hh+":"+mm;
+};
+
+const el = ref();
+
+onMounted(()=>{
+    el.value.innerHTML = is_open();
+})
 
 </script>
 
@@ -46,7 +122,7 @@ li {
 }
 
 .wrap-shadow {
-    @apply fixed top-0 -z-10 bg-gradient-to-b  from-darkYellow via-transparent to-transparent w-screen h-screen
+    @apply fixed top-0 -z-10 bg-gradient-to-b from-darkYellow via-transparent to-transparent w-screen h-screen
 }
 </style>
 
