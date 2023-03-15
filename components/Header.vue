352a72e56e7cd7b04dfce001dbc922608b4dbfda
emanuele-em <micheletti.emanuele@hotmail.com>
2023-03-15 15:10:42 +0100

    udpate info e styles

--- components/Header.vue
+++ components/Header.vue
@@ -3,9 +3,9 @@
         <img :src="`/img/logo.png`" class="w-32 drop-shadow-lg" :alt="`${conf.bgImageAlt}`" />
     </div>
     <h1 :class="` font-medium logoFont capitalize container text-center text-4xl md:text-4xl w-screen py-6 mx-auto ${ conf.bgLight ? 'text-textDark' : 'text-textLight' }`">
-        {{ studioName }}
+       STUDIO PAGLIASSOTTO RIGALDO <br> 
     </h1>
-
+    <i :class="` font-calibri text-3xl m-auto text-center block -mt-5 ${ conf.bgLight ? 'text-textDark' : 'text-textLight' } `">COMMERCIALISTI</i>
     <div class="fixed -z-10 top-0 bottom-0 left-0 right-0  bg-gradient-to-b from-darkYellow to-lightGreen h-screen w-screen">
         <img v-if="conf.bgImage" :src="`/img/${conf.bgImage}`" class="object-cover h-full w-full" :alt="`${conf.bgImageAlt}`" />
     </div>
@@ -13,7 +13,6 @@
 </template>
 
 <script setup>
-    const studioName = "Pagliassotto Rigaldo";
     const { conf } = defineProps(['conf']);
 </script>
 
